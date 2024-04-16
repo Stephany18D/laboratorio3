00c5dba3d6639e0ed79852a5da7c7dc61f9c0588
Stephany18D <111327459+Stephany18D@users.noreply.github.com>
2024-04-15 22:03:37 -0500

    problemas

---
+++ problemas.h
@@ -0,0 +1,9 @@
+#ifndef PROBLEMAS_H
+#define PROBLEMAS_H
+#include "cuentacorriente.h"
+int ejercicio1();
+int ejercicio2();
+int ejercicio3();
+int ejercicio4();
+int ejercicio5();
+#endif // PROBLEMAS_H
